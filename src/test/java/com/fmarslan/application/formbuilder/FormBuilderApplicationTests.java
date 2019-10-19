90f1caf89d5ab19cc372073c18edad67e6ee375d
Fatih Mehmet ARSLAN <iletisim@fmarslan.com>
2019-10-19 19:41:09 +0300

    Add files via upload

---
+++ src/test/java/com/fmarslan/application/formbuilder/FormBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmarslan.application.formbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
